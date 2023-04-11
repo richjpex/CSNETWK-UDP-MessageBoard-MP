276ec778decd57fded759f0ea726b9c27f40ef33
Rich Pecson <richard_pecson@dlsu.edu.ph>
2023-04-12 00:55:08 +0800

    Final Final CSNETWK MP

---
+++ S13-GROUP21-ChatClient.py
@@ -0,0 +1,183 @@
+# Richard John Pecson Jr.
+# Jose Lorenzo Santos
+# CSNETWK - S13
+
+import socket, json, threading
+from art import *
+
+current_handle = None
+# Create a function to send JSON commands to server
+def send_json():
+    
+    global current_handle
+    #current_handle = None
+    while True:
+        # get user input
+        user_input = input()
+        registered = False
+        # parse user input
+        command_list = user_input.split()
+        command = command_list[0].lower()
+        # create JSON object based on user input
+        json_obj = {}
+
+        # Send JSON commands to server
+        if command == '/register':
+            if len(command_list) == 2:
+                if current_handle is None:
+                    json_obj['command'], json_obj['handle'] = 'register', command_list[1]
+                    current_handle = command_list[1]
+                else:
+                    json_obj['command'],  json_obj['message'] = 'error', 'ALREADY-REGISTERED'
+                    current_handle = current_handle
+
+            else:
+                json_obj['command'],  json_obj['message'] = 'error', 'INVALID-PARAMETERS'
+            
+            json_str = json.dumps(json_obj)
+            sock.sendall(json_str.encode())
+
+        elif command == '/all':
+            if len(command_list) >= 2:
+                json_obj['command'], json_obj['message'] = 'all', ' '.join(command_list[1:])
+                
+            else:
+                json_obj['command'], json_obj['message'] = 'error', 'INVALID-PARAMETERS'
+            
+            json_str = json.dumps(json_obj)
+            sock.sendall(json_str.encode())
+
+        elif command == '/msg':
+            if len(command_list) >= 3:
+                json_obj['command'], json_obj['handle'], json_obj['message'] = 'msg', command_list[1], ' '.join(command_list[2:])
+            else:
+                json_obj['command'], json_obj['message'] = 'error', 'INVALID-PARAMETERS'
+
+            json_str = json.dumps(json_obj)
+            sock.sendall(json_str.encode())
+
+        elif command == '/leave':
+            if len(command_list) == 1:
+                json_obj['command'] = 'leave'
+                json_str = json.dumps(json_obj)
+                sock.sendall(json_str.encode())
+                print('\n' * 50)
+                tprint("Message Board")
+                print("Connection closed. Thank you!")
+                login()
+                return False
+                
+            else:
+                json_obj['command'], json_obj['message']  = 'error', 'INVALID-PARAMETERS'
+                json_str = json.dumps(json_obj)
+                sock.sendall(json_str.encode())
+            
+        elif command == '/?' and len(command_list) == 1:
+            print("Commands:")
+            print("/register <handle>")
+            print("/all <message>")
+            print("/msg <handle> <message>")
+            print("/leave")
+
+        # unknown command
+        else:
+            json_obj['command'], json_obj['message'] = 'error', 'UNKNOWN-COMMAND'
+            json_str = json.dumps(json_obj)
+            sock.sendall(json_str.encode())
+
+# Create a function to listen for messages from server
+def listen_for_messages():
+    global current_handle
+    while True:
+        # Receive data from the server
+        try:
+            data = sock.recv(1024)
+            if not data:
+                break
+            # Decode JSON data
+            json_obj = json.loads(data.decode())
+            # Print messages from server
+            if json_obj['command'] == 'register':
+                #print(f"Welcome {json_obj['handle']}!")
+                print(f"{json_obj['message']}")
+
+            elif json_obj['command'] == 'all':
+                print(f"{json_obj['handle']}: {json_obj['message']}")
+
+            elif json_obj['command'] == 'msg':
+                print(f"{json_obj['message']}")
+
+            elif json_obj['command'] == 'error':
+                print(f"{json_obj['message']}")
+                current_handle = None
+
+            elif json_obj['command'] == 'leave':
+                print("Disconnected from server.")
+                return False
+            
+        except ConnectionResetError:
+            print("Error: Connection to the Message Board Server has failed!")
+        
+
+def login():
+    global current_handle
+    while True:
+        
+        user_input = input()
+
+        # Parse user input
+        command_list = user_input.split()
+        command = command_list[0].lower()
+
+        # Create JSON object based on user input
+        json_obj = {}
+
+        # Check if user input is valid
+        if command == '/join' and len(command_list) == 3:
+            host = command_list[1]
+            port = command_list[2]
+            if host == 'localhost' or host == '127.0.0.1' and port == '12345':
+                # Connect the socket to the port where the server is listening
+                server_address = (host, int(port))
+                sock.connect(server_address)
+
+                print('\n' * 50)
+                tprint("Welcome!")
+
+                # Send JSON command
+                json_obj['command'] = 'join'
+                json_str = json.dumps(json_obj)
+                sock.sendall(json_str.encode())
+
+                print("Connection to the Message Board Server is successful!")
+
+                # Create threads
+                send_thread = threading.Thread(target=send_json)
+                listen_thread = threading.Thread(target=listen_for_messages)
+                # Start threads
+                send_thread.start()
+                listen_thread.start()
+                # Join threads
+                send_thread.join()
+                listen_thread.join()
+            else:
+                print("Error: Connection to the Message Board Server has failed! Please check IP Address and Port Number.")
+
+        elif len(command_list) != 3 and command != '/?':
+            print("Error: Command parameters do not match or is not allowed.")
+
+        elif command == '/leave' and len(command_list) == 1:
+            print("Error: Disconnection failed. Please connect to the server first.")
+            
+        elif command == '/?' and len(command_list) == 1:
+            print("Commands:")
+            print("/join <host> <port>")
+        else:
+            print("Error: Command not found.")
+
+if __name__ == '__main__':
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    print('\n' * 50)
+    tprint("Message Board")
+    login()
---
+++ S13-GROUP21-ChatServer.py
@@ -0,0 +1,116 @@
+# Richard John Pecson Jr.
+# Jose Lorenzo Santos
+# CSNETWK - S13
+import socket, json
+
+# create UDP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# bind socket to specific IP address and port
+server_address = ('127.0.0.1', 12345)
+sock.bind(server_address)
+print(f"Server created at {server_address}")
+
+# create dictionary of registered handles
+handles = {}
+
+while True:
+    # receive data from the client
+    data, address = sock.recvfrom(1024)
+
+    # decode the received data as a JSON object
+    try:
+        json_data = json.loads(data.decode('utf-8'))
+
+    except json.JSONDecodeError as e:
+        error_message = {'command': 'error', 'message': str(e)}
+        sock.sendto(json.dumps(error_message).encode('utf-8'), address)
+        continue
+
+    #process the JSON command
+    json_command = json_data.get('command', '').lower()
+
+    # if command is /join
+    if json_command == 'join':
+        print(f'Client {address} has joined.')
+
+    # if command is /leave
+    elif json_command == 'leave':
+        print(f'Client {address} has disconnected.')
+
+        # delete the handle from the handles dictionary of the client that left
+        for key, value in handles.items():
+            if value == address:
+                del handles[key]
+                print(f"Current handles: {handles}")
+                break
+
+    # if command is /register
+    elif json_command == 'register':
+        # register a unique handle
+        handle = json_data.get('handle', '')
+
+        if handle in handles:
+            error_message = {'command': 'error', 'message': f'Error: Registration failed. Handle or alias already exists.'}
+            sock.sendto(json.dumps(error_message).encode('utf-8'), address)
+        
+        else:
+            handles[handle] = address
+            print(handles)
+            response = {'command': 'register', 'handle': handle, 'message': f'You are now registered as "{handle}".'}
+            sock.sendto(json.dumps(response).encode('utf-8'), address)
+            print("Welcome " + handle + "!")
+
+    # if command is /all
+    elif json_command == 'all':
+        # send message to all clients
+        # get handle based on the addr from the client
+        for key, value in handles.items():
+            if value == address:
+                user_handle = key
+                
+        message = json_data.get('message', '')
+        for handle, client_address in handles.items():
+            response = {'command': 'all', 'handle': user_handle, 'message': message}
+            sock.sendto(json.dumps(response).encode('utf-8'), client_address)
+
+            
+
+    # if command is /msg
+    elif json_command == 'msg':
+        # send direct message to a single handle
+        # get the handle from the handles dictionary based on the addr from the client
+        for key, value in handles.items():
+            if value == address:
+                user_handle = key
+
+        handle = json_data.get('handle', '')
+        if handle not in handles:
+            error_message = {'command': 'error', 'message': f'Error: Handle or alias "{handle}" not found'}
+            sock.sendto(json.dumps(error_message).encode('utf-8'), address)
+        else:
+            message = json_data.get('message', '')
+
+            recipient_response = {'command': 'msg', 'handle': user_handle, 'message': f'[From {user_handle}]: {message}'}
+            sock.sendto(json.dumps(recipient_response).encode('utf-8'), handles[handle])
+
+            sender_response = {'command': 'msg', 'handle': handle, 'message': f'[To {handle}]: {message}'}
+            sock.sendto(json.dumps(sender_response).encode('utf-8'), address)
+
+    # if command is error
+    elif json_command == "error":
+        # invalid param
+        if (json_data.get('message', '')) == 'INVALID-PARAMETERS':
+            error_message = {'command': 'error', 'message': 'Error: Command parameters do not match or is not allowed.'}
+        # unknown command
+        elif (json_data.get('message', '')) == 'UNKNOWN-COMMAND':
+            error_message = {'command': 'error', 'message': 'Error: Command not found.'}
+        # handle already exists
+        elif (json_data.get('message', '')) == 'ALREADY-REGISTERED':
+            error_message = {'command': 'error', 'message': 'Error: You are already registered.'}
+        
+        sock.sendto(json.dumps(error_message).encode('utf-8'), address)
+
+
+
+sock.close()
